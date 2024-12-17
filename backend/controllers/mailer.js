dcd905916293c2e5b56d64cd2bd63e15ed61a537
Alemu2502 <fre6819@gnail.com>
2024-12-17 00:39:35 -0800

    updated

--- backend/controllers/mailer.js
+++ backend/controllers/mailer.js
@@ -6,6 +6,7 @@ dotenv.config(); // Load environment variables
 export const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
